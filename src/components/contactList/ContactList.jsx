e7c0147de3685e8f3db820fe11b261b0cb9753a2
Tomashev Vladyslav <vladmassive@gmail.com>
2024-01-11 07:26:15 +0100

    Sum 10.11

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import css from '../App.module.css';
 
-export const ContactList = ({ onDelit }) => {
-  const contacts = useSelector(state => state.contacts.items);
+export const ContactList = ({ onDelit, contacts }) => {
+  // const contacts = useSelector(state => state.contacts.items);
   return (
     <ul>
-      {contacts &&
-        contacts.map(({ id, name, phone }) => {
-          return (
-            <li key={id}>
-              <span>{name}</span>: <b>{phone}</b>{' '}
-              <button
-                className={css.add_btn}
-                type="button"
-                onClick={() => onDelit(id)}
-              >
-                Remove
-              </button>
-            </li>
-          );
-        })}
+      {/* {contacts && */}
+      {contacts.map(({ id, name, phone }) => {
+        return (
+          <li key={id}>
+            <span>{name}</span>: <b>{phone}</b>{' '}
+            <button
+              className={css.add_btn}
+              type="button"
+              onClick={() => onDelit(id)}
+            >
+              Remove
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 };
